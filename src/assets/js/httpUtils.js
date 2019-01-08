400360ee760e835866aafda4b545668f8a64dae5
liukai01 <471091127@qq.com>
2019-01-08 23:50:35 +0800

    常见的http提交形式

--- src/assets/js/httpUtils.js
+++ src/assets/js/httpUtils.js
@@ -7,18 +7,15 @@ export default {
     install(Vue, { axios, qs }){
         function http() {
             // GET请求
-            this.get = function(url,data,wc=true) {
+            this.get = function(url,wc=true) {
                 return new Promise((resolve,reject)=>{
                     axios({
                         method: 'get',
                         url,
-                        withCredentials: wc,
-                        headers: {
-                          'content-type': 'application/x-www-form-urlencode;'
-                        }
+                        withCredentials: wc
                     }).then(res=>{
                         if(res.data){
-                          return resolve(res.data)
+                          return resolve(res)
                         }else{
                          return reject(res)
                         }
@@ -28,14 +25,16 @@ export default {
                 })              
             }
             // json格式提交
-            this.postJSON = function(url,data={},wc=true) {
+            this.postJSON = function(url,data={}, wc=true) {
                 return new Promise((resolve, reject) => {
                   axios({
                     method: 'post',
                     url,
                     data,
-                    withCredentials: true,
-                    headers: { 'Content-Type': 'application/json' }
+                    withCredentials: wc,
+                    headers: {
+                      'Content-Type': 'application/json' 
+                    }
                   }).then(res => {
                     return resolve(res)
                   }).catch(err => {
@@ -50,8 +49,10 @@ export default {
                     method: 'post',
                     url,
                     data:qs.stringify(data),
-                    withCredentials: true,
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+                    withCredentials: wc,
+                    headers: { 
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                    }
                   }).then(res => {
                     return resolve(res)
                   }).catch(err => {
@@ -70,7 +71,7 @@ export default {
                     method: 'post',
                     url,
                     data: formData,
-                    withCredentials: true,
+                    withCredentials: wc,
                   }).then(res => {
                     return resolve(res)
                   }).catch(err => {
@@ -85,8 +86,10 @@ export default {
                     method: 'post',
                     url,
                     data,
-                    withCredentials: true,
-                    headers: { 'Content-Type': 'text/xml' }
+                    withCredentials: wc,
+                    headers: { 
+                      'Content-Type': 'text/xml'
+                    }
                   }).then(res => {
                     return resolve(res)
                   }).catch(err => {
